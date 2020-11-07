6fd93a1712571a59673619af66d05bc482344230
katalepsy <janbres666@gmail.com>
2020-11-07 13:38:39 +0800

    3.3

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_ui/screens/home_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Chat UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.red,
+        accentColor: Color(0xFFFEF9EB),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
